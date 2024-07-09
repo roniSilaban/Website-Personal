6f886168dbd877ddfe033c19b23d34fd2aef7ac9
roniSilaban <145220652+roniSilaban@users.noreply.github.com>
2024-07-09 23:43:28 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Website-Personal
-Web
+# Toko Online Roti
+Project Toko Online Roti Lengkap dengan proses manufacturing
+
+UNTUK MASUK HALAMAN ADMIN SILAHKAN MASUK DENGAN MENAMBAHKAN /admin PADA URL
---
+++ footer.php
@@ -0,0 +1,31 @@
+
+	<footer style="border-top: 4px solid #1af84aee;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #1af84aee"><b>AROA-TRADISIONAL SNACK</b></h3>
+					<p>Jl. Tanah Merah Indah 1 No.10C</p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +6281343992035</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> aroasnack@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href=""  style="color: #000">Produk</a></p>
+					<p><a href=""  style="color: #000">Tentang kami</a></p>
+					<p><a href=""  style="color: #000">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: #1af84aee; padding: 5px; color: #fff; text-align: center;">
+			<span>Copyright&copy; Adisti,Roni,Adkhi</span>
+		</div>
+	</footer>
+
+</body>
+</html>
